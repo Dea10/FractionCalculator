c50e69ba43cba2b7f9ab96e40b1d178482951d42
Daniel Ubuntu home <d111092.de@gmail.com>
2019-07-21 12:59:36 -0500

    equals, toLowestTerms and gcd methods added

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,7 +1,7 @@
 public class Fraction {
 
-    private int numerator;
-    private int denominator;
+    public int numerator;
+    public int denominator;
 
             /* --- Constructors --- */
 
@@ -20,9 +20,6 @@ public class Fraction {
             this.numerator = numerator * -1;
             this.denominator = denominator * -1;
         }
-
-        System.out.println("Num: " + this.numerator);
-        System.out.println("Den: " + this.denominator);
     }
 
     public Fraction(int numerator) {
@@ -34,7 +31,7 @@ public class Fraction {
     }
 
             /* --- Methods --- */
-
+/*
     public int getNumerator() {}
 
     public int getDenominator() {}
@@ -50,10 +47,33 @@ public class Fraction {
     public Fraction multiply(Fraction other) {}
 
     public Fraction divide(Fraction other) {}
+*/
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Fraction) {
+            if((double)((Fraction) other).numerator/(double)((Fraction) other).denominator != (double)this.numerator/(double)this.denominator){
+                return false;
+            }else {
+                return true;
+            }
+        }else {
+            return false;
+        }
+    }
 
-    public boolean equals(Object other) {}
+    public void toLowestTerms() {
+        int numeratorTemp = this.numerator;
+        this.numerator = this.numerator / gcd(numeratorTemp, this.denominator);
+        this.denominator = this.denominator / gcd(numeratorTemp, this.denominator);
+    }
 
-    public void toLowestTerms() {}
-
-    public int gcd(int num, int den) {}
+    public static int gcd(int num, int den) {
+        int remainder;
+        while (num != 0 && den != 0) {
+            remainder = num % den;
+            num = den;
+            den = remainder;
+        }
+        return num;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        Fraction myFrac = new Fraction(105, 147);
+        Fraction otherFrac = new Fraction(5, 7);
 
+        System.out.println("Num: " + myFrac.numerator);
+        System.out.println("Den: " + myFrac.denominator);
+
+        System.out.println("Equals: " + myFrac.equals(4));
+        System.out.println("Equals: "+ myFrac.equals(otherFrac));
+
+        myFrac.toLowestTerms();
+
+        System.out.println("tlt Num: " + myFrac.numerator);
+        System.out.println("tlt Den: " + myFrac.denominator);
     }
 }
